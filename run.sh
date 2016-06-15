a9121a6f16908a0b46dee9d0e57e803eba1420eb
Peter Rubenstein <peter.rubenstein@teamaol.com>
2016-06-15 17:28:00 -0400

    run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 # Run from within the directory with the Dockerfile, NOT your home dir
 # Change DOCKERCMD to meet your needs
-DOCKERCMD='ksu -k -e /usr/bin/docker'
-cp -R ~/.ssh .
+
+# DOCKERCMD='ksu -k -e /usr/bin/docker'
+DOCKERCMD='/usr/local/bin/docker'
+cp -R ~/.ssh . && \
 $DOCKERCMD build -t iperl . && \
-$DOCKERCMD run --name=iperl --rm -p 8888:8888 iperl && \
+$DOCKERCMD run --name=iperl --rm -p 8888:8888 -v /Users/prubenstein1/notebooks:/notebooks iperl && \
 rm -rf ./.ssh
