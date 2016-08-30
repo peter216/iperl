e4a8d5be8a964beba78ebbd8e3df71669543e197
Peter Rubenstein <peter.rubenstein@teamaol.com>
2016-08-30 13:46:38 -0400

    Changed to reference dockerhub

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 
 # DOCKERCMD='ksu -k -e /usr/bin/docker'
 DOCKERCMD='/usr/local/bin/docker'
-$DOCKERCMD build -t iperl . && \
-$DOCKERCMD run -it --name=iperl --rm -p 8889:8888 -v /Users/prubenstein1/notebooks:/notebooks iperl
+$DOCKERCMD pull martiangoblin/iperl && \
+$DOCKERCMD run -it --name=iperl --rm -p 8889:8888 -v /Users/prubenstein1/notebooks:/notebooks martiangoblin/iperl
